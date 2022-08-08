95e6b4f0c5cf0c90639a79d41c694e3d3c6a24e7
Juan C Calix <j_calix2002@hotmail.com>
2022-08-07 19:15:19 -0700

    add Sidebar and add NavBar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,8 +1,20 @@
 import {Typography} from  '@mui/material'
+import {JournalLayout} from  '../layout/JournalLayout'
 
 export const JournalPage =() => {
     return(
-        <Typography  component ='h1'> JournalPage</Typography>
+        <JournalLayout>
+            <Typography>
+Dolore consequat culpa proident sint laborum deserunt eu sit. Adipisicing ipsum do officia nulla tempor mollit magna duis do nulla pariatur dolore aute velit. Fugiat esse laborum adipisicing anim ut.
+
+Id elit commodo laboris aute occaecat aliqua voluptate qui proident do aliquip elit aute excepteur. Deserunt consectetur fugiat Lorem sint ea culpa cupidatat voluptate. Enim qui consequat cillum qui quis quis non aliquip ut. Ex amet veniam incididunt cupidatat eiusmod ex proident ad incididunt. Non aliqua officia reprehenderit laborum proident reprehenderit eiusmod laborum.
+
+Do ea magna proident cupidatat aliquip laborum minim eiusmod occaecat esse. Incididunt nulla eiusmod reprehenderit adipisicing velit incididunt sunt fugiat qui duis reprehenderit dolor. Consectetur enim aliqua dolor occaecat nulla aute. Enim eiusmod proident ipsum quis enim. Aliquip reprehenderit dolore eiusmod nostrud nisi irure dolore et occaecat sit voluptate aute.
+
+
+            </Typography>
+        </JournalLayout>
+       
         
     )
 }
